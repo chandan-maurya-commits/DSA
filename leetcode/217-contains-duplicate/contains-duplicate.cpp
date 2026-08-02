9ead83875b349eb0957cf2d596f3a69c19875988
chandan-maurya-commits <cdnmry@gmail.com>
2026-08-02 23:59:00 +0530

    Time: 75 ms (30.86%) | Memory: 113.4 MB (11.96%) - LeetSync

--- leetcode/217-contains-duplicate/contains-duplicate.cpp
+++ leetcode/217-contains-duplicate/contains-duplicate.cpp
@@ -1,14 +1,45 @@
+// using unordered set 
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         unordered_set<int> st;
+
+//         for(int num : nums){
+//             if(st.find(num) != st.end()){
+//                 return true;
+//             }
+//             st.insert(num);
+//         }
+//         return false;
+//     }
+// };
+
+
+// using unordered set based on size
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> st;
+        unordered_set<int> st(nums.begin(), nums.end());
 
-        for(int num : nums){
-            if(st.find(num) != st.end()){
-                return true;
-            }
-            st.insert(num);
+        if(nums.size() > st.size()){
+            return true;
         }
         return false;
     }
 };
+
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         unordered_set<int> st;
+
+//         for(int num : nums){
+//             if(st.find(num) != st.end()){
+//                 return true;
+//             }
+//             st.insert(num);
+//         }
+//         return false;
+//     }
+// };
