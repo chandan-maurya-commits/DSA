9629d5aea080b96489129854539ab11a8ffb0ef2
chandan-maurya-commits <cdnmry@gmail.com>
2026-08-03 00:40:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (50.14%) - LeetSync

--- leetcode/242-valid-anagram/valid-anagram.cpp
+++ leetcode/242-valid-anagram/valid-anagram.cpp
@@ -12,47 +12,47 @@
 // };
 
 //   using frequency array
-//  class Solution {
-//  public:
-//      bool isAnagram(string s, string t) {
-//         if(s.size() != t.size()){
-//             return false;
-//         }
-//         vector<int> freq(26,0);
-//         for(char c:s){
-//             freq[c -'a']++;
-//         }
-//         for(char c : t){
-//             freq[c - 'a']--;
-//         }
-//         for(int count :freq){
-//             if(count != 0){
-//                 return false;
-//             }
-//         }
-//         return true;
-//      }
-// };
-
-//   using hashmap
  class Solution {
  public:
      bool isAnagram(string s, string t) {
         if(s.size() != t.size()){
             return false;
         }
-        unordered_map<char,int> mp;
-
-        for(char c : s){
-            mp[c]++;
+        vector<int> freq(26,0);
+        for(char c:s){
+            freq[c -'a']++;
         }
         for(char c : t){
-            mp[c]--;
-            if(mp[c] <0){
+            freq[c - 'a']--;
+        }
+        for(int count :freq){
+            if(count != 0){
                 return false;
             }
         }
         return true;
-         
      }
- };
+};
+
+// //   using hashmap
+//  class Solution {
+//  public:
+//      bool isAnagram(string s, string t) {
+//         if(s.size() != t.size()){
+//             return false;
+//         }
+//         unordered_map<char,int> mp;
+
+//         for(char c : s){
+//             mp[c]++;
+//         }
+//         for(char c : t){
+//             mp[c]--;
+//             if(mp[c] <0){
+//                 return false;
+//             }
+//         }
+//         return true;
+         
+//      }
+//  };
