b1f357de84c27494bfc96a36c0ef4a6e6ae2c808
chandan-maurya-commits <cdnmry@gmail.com>
2026-08-06 20:41:46 +0530

    Time: 41 ms (63.12%) | Memory: 45.6 MB (40.13%) - LeetSync

--- leetcode/560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ leetcode/560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -4,15 +4,14 @@ public:
         unordered_map<int, int> mp;
         mp[0] = 1;
 
-        int prefix_sum = 0;
+        int sum = 0;
         int count = 0;
-
         for(int num : nums){
-            prefix_sum += num;
-            if(mp.find(prefix_sum - k) != mp.end()){
-                count += mp[prefix_sum - k];
+            sum += num;
+            if(mp.find(sum-k) != mp.end()){
+                count += mp[sum-k];
             }
-            mp[prefix_sum]++;
+            mp[sum]++;
         }
         return count;
     }
