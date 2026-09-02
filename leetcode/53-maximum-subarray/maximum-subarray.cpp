06be0ac2d9b6a0bd32766ade220753d7c9da03c9
chandan maurya <cdnmry@gmail.com>
2026-09-02 21:03:44 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (52.75%) - LeetSync

--- leetcode/53-maximum-subarray/maximum-subarray.cpp
+++ leetcode/53-maximum-subarray/maximum-subarray.cpp
@@ -1,14 +1,87 @@
+// BRUTE gives TLE
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& nums) {
+      
+    //   int maxSum = INT_MIN;
+       
+    //    for(int i=0; i<nums.size(); i++){
+    //         for(int j=i; j<nums.size(); j++){
+    //              int sum = 0;
+    //             for(int k=i; k<=j; k++ ){
+    //                 sum += nums[k];
+                   
+    //             }
+    //              maxSum = max(sum, maxSum);
+    //         }
+    //    }
+//        return maxSum;
+//     }
+// };
+
+// BETTER gives TLE
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& nums) {
+      
+      
+//       int maxSum = INT_MIN;
+       
+//        for(int i=0; i<nums.size(); i++){
+//          int sum = 0;
+//             for(int j=i; j<nums.size(); j++){
+                
+//                 sum += nums[j];
+//                 maxSum = max(sum, maxSum);
+//             }
+             
+//        }
+//              return maxSum;
+//     }
+// };
+
+
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& nums) {
+      
+      
+//       int maxSum = INT_MIN;
+       
+//        for(int i=0; i<nums.size(); i++){
+//          int sum = 0;
+//             for(int j=i; j<nums.size(); j++){
+                
+//                 sum += nums[j];
+//                 maxSum = max(sum, maxSum);
+//             }
+             
+//        }
+//              return maxSum;
+//     }
+// };
+
+
+// OPTIMAL (KADANE'S ALGO..)
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
       
-       int maxSum = INT_MIN;
-       int currSum = 0;
-       for(int i=0; i<nums.size(); i++){
-          currSum = max(nums[i], currSum+nums[i]);
-          maxSum = max(currSum, maxSum);
-       }
-       return maxSum;
+      
+      int maxSum = INT_MIN;
+      int sum = 0;
+      for(int i=0; i<nums.size(); i++){
+        sum += nums[i];
 
+        maxSum = max(sum, maxSum);
+
+        if(sum < 0){
+            sum = 0;
+        }
+        
+      }
+      return maxSum;
     }
+       
+       
 };
