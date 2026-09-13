bf954f86a54b35a942bf68aa395cc546aad34501
chandan maurya <cdnmry@gmail.com>
2026-09-14 00:05:56 +0530

    Time: 0 ms (100.00%) | Memory: 128.2 MB (78.26%) - LeetSync

--- leetcode/2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ leetcode/2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,53 +1,53 @@
-// class Solution {
-// public:
-//     vector<int> rearrangeArray(vector<int>& nums) {
-//         int n = nums.size();
-//         vector<int> ans(n);
-
-//         int pos = 0;
-//         int neg = 1;
-
-//         for(auto num : nums){
-//             if(num > 0){
-//                 ans[pos] = num;
-//                 pos += 2;
-//             }else{
-//                 ans[neg] = num;
-//                 neg += 2;
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
-
-// Brute.
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> pos;
-        vector<int> neg;
+        int n = nums.size();
+        vector<int> ans(n);
 
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] < 0){
-                neg.push_back(nums[i]);
+        int pos = 0;
+        int neg = 1;
+
+        for(auto num : nums){
+            if(num > 0){
+                ans[pos] = num;
+                pos += 2;
             }else{
-                pos.push_back(nums[i]);
+                ans[neg] = num;
+                neg += 2;
             }
         }
-        // int idx = 0;
-        int p=0;
-        int n=0;
-        for(int i=0; i<nums.size(); i++){
-            if(i%2 == 0){
-                nums[i] = pos[p];
-                p++;
-            }else{
-                nums[i] = neg[n];
-                n++;
-            }
-        }
-        return nums;
-        
+        return ans;
     }
 };
+
+
+// // Brute.
+// class Solution {
+// public:
+//     vector<int> rearrangeArray(vector<int>& nums) {
+//         vector<int> pos;
+//         vector<int> neg;
+
+//         for(int i=0; i<nums.size(); i++){
+//             if(nums[i] < 0){
+//                 neg.push_back(nums[i]);
+//             }else{
+//                 pos.push_back(nums[i]);
+//             }
+//         }
+        
+//         int p=0;
+//         int n=0;
+//         for(int i=0; i<nums.size(); i++){
+//             if(i%2 == 0){
+//                 nums[i] = pos[p];
+//                 p++;
+//             }else{
+//                 nums[i] = neg[n];
+//                 n++;
+//             }
+//         }
+//         return nums;
+        
+//     }
+// };
