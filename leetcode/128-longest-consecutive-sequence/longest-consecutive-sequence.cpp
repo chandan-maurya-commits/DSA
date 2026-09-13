63a54f3dc542335c4ba4c03328f1d63f469b9d2a
chandan maurya <cdnmry@gmail.com>
2026-09-14 02:40:16 +0530

    Time: 3 ms (99.92%) | Memory: 60.2 MB (93.09%) - LeetSync

--- leetcode/128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ leetcode/128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,23 +1,65 @@
+
+// Brute
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) {
+//         int maxCount = 0;
+        
+
+//        for(int i=0; i<nums.size(); i++){
+//             int x= nums[i];
+//             int count = 1;
+//             while(true){
+//                 bool found = false;
+//                 for(int j=0; j<nums.size(); j++){
+//                     if(nums[j] == x+1){
+//                         x++;
+//                         count++;
+//                         found = true;
+//                         break;
+//                     }
+//                 }
+//                 if(found == false){
+//                     break;
+//                 }
+//             }
+//             maxCount = max(count, maxCount);
+            
+//        }
+//        return maxCount;
+            
+//     }
+// };
+
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> st(nums.begin(), nums.end());
-        int longest = 0;
 
-
-        for(int num : st){
-            if(st.find(num-1) == st.end()){
-                int current = num;
-                int length = 1;
-            
-
-            while(st.find(current+1) != st.end()){
-                current++;
-                length++;
-            }
-            longest = max(length,longest);
-            }
+        if(nums.size() == 0){
+            return 0;
         }
-        return longest;
+        if(nums.size() == 1){
+            return 1;
+        }
+        sort(nums.begin(), nums.end());
+
+        int maxCount = 0;
+        int x = nums[0];
+        int count = 1;
+        for(int i=1; i<nums.size(); i++){
+            
+            if(nums[i] == x+1 ){
+                count++;
+                x++;
+            }else if(nums[i] == x){
+
+            }else{
+                count = 1;
+                x = nums[i];
+            }
+            maxCount = max(count, maxCount);
+        }
+        return maxCount;
+            
     }
 };
